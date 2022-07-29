38dc5cd02a5b118dcc48fea5a13201cb0173f19c
Vlad Shovkoplias <polosatui101@gmail.com>
2022-07-29 18:04:19 +0300

    hw11

--- src/LambdaTest.java
+++ src/LambdaTest.java
@@ -15,8 +15,10 @@ public class LambdaTest {
                 {-1.0, 10.0, 10.0},
                 {0.0, 0.0, 0.0},
                 {-10.0, NaN, NaN},
-                {POSITIVE_INFINITY, 1e32, POSITIVE_INFINITY}
+                {POSITIVE_INFINITY, 1e32, POSITIVE_INFINITY},
                 // TODO add 2 more test data here
+                {10.0, -30.0, 10.0},
+                {1.7, 1.8, 1.8}
         };
     }
 
@@ -25,8 +27,10 @@ public class LambdaTest {
         return new Object[][]{
                 {25.0, 5.0},
                 {-1.0, NaN},
-                {POSITIVE_INFINITY, POSITIVE_INFINITY}
+                {POSITIVE_INFINITY, POSITIVE_INFINITY},
                 // TODO add 2 more test data here
+                {9.0, 3.0},
+                {64.0, 8.0}
         };
     }
 
